12fcc1c896bbc58456a0c4c40f9803169ae0c5d4
carlos <kadusouza999@gmai.com>
2026-01-19 17:16:38 -0300

    Primeira parte da API 2

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+const users = [];
+
+app.post("/usuario", (req, res) => {
+  users.push(req.body);
+
+  res.status(201);
+});
+
+app.get("/usuario", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.listen(3004);
