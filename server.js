395bd39aa0d469ff611ccd87a0c017a1ce613200
carlos <kadusouza999@gmai.com>
2026-01-20 14:19:46 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 import express from "express";
+import { pool } from "./database.js";
 
 const app = express();
 app.use(express.json());
-const users = [];
 
-app.post("/usuario", (req, res) => {
-  users.push(req.body);
+app.post("/usuario", async (req, res) => {
+  const { name, email } = req.body;
 
-  res.status(201);
+  await pool.query("INSERT INTO user (name, email) VALUES (?, ?)", [
+    name,
+    email,
+  ]);
+
+  res.status(201).send("Salvo no MySQL");
 });
 
-app.get("/usuario", (req, res) => {
-  res.status(200).json(users);
+app.get("/usuario", async (req, res) => {
+  const result = await pool.query("SELECT * FROM usuarios");
+  res.status(200).json(result.rows);
 });
 
-app.listen(3004);
+app.listen(3004, () => {
+  console.log("Servidor rodando na porta 3004");
+});
